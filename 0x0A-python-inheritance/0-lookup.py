b2fbf2cc63dcac89db9b610b7eb58a36511347d0
Dkmbugua <dkmbugua70@gmail.com>
2024-06-11 12:46:10 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+"""
+function lookuo take a single parameter whoe attributes and methods you
+to retrieve
+"""
+
+def lookup(obj):
+    """Return the list of availabe attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,4 @@
+class Mylist(list):
+    def print_sorted(self):
+        """a method uses the sorted function tosort list in ascending order"""
+        print(sorted(list(self)))
