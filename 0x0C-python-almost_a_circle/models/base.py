aedb399f93688a12fd6592941e533bd9ccec0d2d
Dkmbugua <dkmbugua70@gmail.com>
2024-06-17 06:55:05 -0700

    almost a cirlce

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,3 +11,20 @@ class Base:
             """"if id is none __nb_object is incremented"""
             __nb_objects =+1
             self.id = __nb_objects
+
+@staticmethod
+def to_json_string(dict_list):
+    """convert a list of dictionaries to a JSON list
+    dict_list is a list of dictionaries.
+    returns JSON string represenation"""
+
+    if not dict_list:
+        return "[]"
+    return json.dumps(dict_list)
+
+@staticmethod
+def from_json_string(json_string):
+    """return deserialization of a json string to a list_dict"""
+    if not json_string:
+        return"[]"
+    return json.load(json_string)
