d78db568bf633945f388b98f42bc434e5ccdca04
Dkmbugua <dkmbugua70@gmail.com>
2024-07-03 03:28:41 -0700

    orm

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
+"""Lists all City objects from the database hbtn_0e_14_usa.
+Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+                                         <mysql password> /
+                                         <database name>
 """
-contains the class City
-"""
 
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-class City(Base):
-    """Representation of a city"""
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
